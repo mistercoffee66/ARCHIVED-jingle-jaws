70ba7b7e6e9bdbe420d3e8f2dc5d81b061d92eab
samelwitt <samelwitt@gmail.com>
2012-02-06 15:41:21 -0500

    Valentines in progress

--- src/rails/app/assets/javascripts/JJ/views/chum.js
+++ src/rails/app/assets/javascripts/JJ/views/chum.js
@@ -1,3 +1,4 @@
+console.log('foo');
 goog.provide("JJ.views.chum");
 goog.exportSymbol("JJ.views.chum.Index"); // export view constructors so views can still consume them after minification
 
@@ -7,7 +8,9 @@ goog.require('JJ.managers.SocketManager');
  * @constructor
  */
 JJ.views.chum.Index = function() {
-	this.initPresentation();
+    JJ.log('initPresentation');
+    console.log('bar');
+    this.initPresentation();
 	this.initServerEvents();
 	this.initClientEvents();
 };
@@ -16,29 +19,32 @@ JJ.views.chum.Index = function() {
 JJ.views.chum.Index.prototype = {
 
     initPresentation: function() {
-		var self = this;
+
+        var self = this;
         this._mainWrap = $('#main-wrap');
 		this._videoWrap = $('#video-wrap');
-		this._porthole = $('#porthole');
-		this._viewport = $('#viewport');
 		this._colLeft = $('#col-left');
 		this._colRight = $('#col-right');
 		this._colMid = $('#col-mid');
 		this._controls = $('#controls');
 		this._logoWrap = $('#logo-wrap');
-		this._ticker = $('#ticker');
-		this._tank = $('#tank');
-		this._shark = $('#shark');
-		this._sleigh = $('#sleigh');
-		this._header = $('#header');
-        this._durationFactor = .88;
         this._theme = document.getElementById('theme');
         this._preLoad = $.Deferred();
 
-		this.preLoadMedia();
+        $(self._mainWrap).addClass('loading');
+        this.preLoadMedia();
         this._preLoad.done(function(){
-            $(self._mainWrap).addClass('intro splash').show(1, $.proxy(self.setVideoSize, self));
-            self.doSplash();
+
+            // enable various clicks to turn on classes for testing
+//            $(self._mainWrap).removeClass('loading').addClass('intro');
+//            $(self._mainWrap).click(function() {
+//                $(this).addClass('splash');
+//            });
+//            $(self._header).click(function() {
+//                $(self._mainWrap).addClass('add-theme');
+//            });
+
+            self.setVideoSize();
             $(window).resize($.proxy(self.setVideoSize, self));
             $(self._logoWrap).click($.proxy(self.showContentState, self));
         });
@@ -62,11 +68,13 @@ JJ.views.chum.Index.prototype = {
         media = [
             '/audio/jinglejaws' + audioType,
             '/assets/bg-main-grad.jpg',
-            '/assets/bg-trees-tile.png',
             '/assets/img-bruce.png',
-            '/assets/img-sleigh-ltr.png',
+            '/assets/img-bruce-theme.png',
+            '/assets/img-swimmer-male.png',
+            '/assets/img-swimmer-female.png',
             '/assets/txt-jaws.png',
-            '/assets/txt-jingle.png'
+            '/assets/moon-bottom.png',
+            '/assets/moon-top.png'
         ];
 
         loader();
@@ -88,141 +96,51 @@ JJ.views.chum.Index.prototype = {
     },
 	
 	setVideoSize: function() {
-		var wrapperDims = {
-				w: $(this._mainWrap).width(),
-				h: $(this._mainWrap).height()
+
+        JJ.log('setVideoSize');
+
+        var self = this,
+            colDims = {
+				w: $(self._colMid).width(),
+				h: $(self._colMid).height()
 			},
-			gutterWidth = $(this._colLeft).width() + $(this._colRight).width(),
-			pAspectRatio = 1.148,
-			pDims = {},
-			vDims = {},
-			pMax = {};
+			aspectRatio = .75,
+			videoDims = {};
 
-		if ($(this._mainWrap).hasClass('intro')) {
-            this.center(this._logoWrap);
-            this.center(this._shark);
-            this.center(this._sleigh);
-            $(this._colMid).css('top', wrapperDims.h);
-        }
+        videoDims.w = colDims.w;
+        videoDims.h = videoDims.hMax = colDims.h - $(self._controls).height() - $(self._header).height();
 
-		$(this._colMid).width(wrapperDims.w - gutterWidth);
-		this.center(this._colMid);
-		
-		pMax.w = wrapperDims.w - gutterWidth;
-		pMax.h = wrapperDims.h - 80;
-		JJ.log(pMax);
 
-		pDims = pMax;
+        if (colDims.h/colDims.w > aspectRatio) {
 
-		if (pMax.w/pMax.h > pAspectRatio) {
-			pDims.w = pMax.h * pAspectRatio;
-			JJ.log('too wide, adjust width');
-		}
-		else if (pMax.w/pMax.h < pAspectRatio) {
-			pDims.h = pMax.w / pAspectRatio;
+            videoDims.h = Math.min( (videoDims.w * aspectRatio), videoDims.hMax);
 			JJ.log('too tall, adjust height');
 		}
+		else if (colDims.h/colDims.w < aspectRatio) {
+            videoDims.w = videoDims.h / aspectRatio;
+            JJ.log('too wide, adjust width');
+		}
 
-		JJ.log(pDims);
-
-		$(this._porthole).width(pDims.w);
-		$(this._porthole).height(pDims.h);
-		$(this._videoWrap).height(pDims.h);
-		$(this._controls).css('top', pDims.h * -.15);
-
-		vDims.w = pDims.w * .84;
-		vDims.h = pDims.w * .84;
-		vDims.t = pDims.w * .12;
-
-		$(this._viewport).css({
-			width: vDims.w,
-			height: vDims.h
-		});
-
-		this.center(this._viewport);
-		this.center($('video'), 'vertical');
+        $(self._videoWrap).width(videoDims.w);
+        $(self._videoWrap).height(videoDims.h);
 
 	},
 
-    doSplash: function() {
+    doIntro: function() {
 
         var self = this;
 
         self._theme.play();
-        $(this._mainWrap).animate({
-            opacity : 1
-        }, 6000*self._durationFactor, function(){
-            $(self._shark).animate({
-                bottom: 0
-            }, 6000*self._durationFactor, function(){
-              $('#logo-jaws').animate({
-                  height: '80%'
-              }, 6000*self._durationFactor, function(){
-                  $(self._mainWrap).removeClass('splash');
-                  self.showContentState();
-              });
-            });
-        });
+
+        // TODO
+
     },
 
-	center: function(ele, dim, context) {
-		context = context || $(ele).parent();
-		dim = dim || 'horizontal';
-
-		switch(dim) {
-			case 'vertical' : JJ.log($(ele)); $(ele).css('top', ($(context).height() - $(ele).height()) / 2); break;
-			case 'horizontal' : $(ele).css('left', ($(context).width() - $(ele).width()) / 2); break;
-		}
-	},
-
 	showContentState: function() {
+        // TODO
 
-        var self = this;
-
-        $(this._sleigh).delay(2000*self._durationFactor).animate({
-			left: $(this._ticker).width() + 150
-		}, 1000, function() {
-            JJ.log('done');
-			$(self._logoWrap).animate({
-				width: 390,
-				left: 102
-			}, 500, function(){
-				$(self._header).animate({
-					height: 157
-				});
-
-				$(self._tank).animate({
-					height: 160
-				}, 500);
-
-				$(self._shark).animate({
-					height: 160,
-					width: 160,
-					left: 102
-				}, 500, function(){
-
-					$('.intro #col-mid').animate({
-						top: -140
-					});
-					$(self._ticker).css({
-						width: '80%',
-						top: 'auto',
-						bottom: 10,
-						left: 262
-					});
-					self.doTicker();
-                    $(self._mainWrap).removeClass('intro');
-				});
-			});
-		});
 	},
 
-	doTicker: function() {
-		$('#ticker-content').html('Well, shake it up, baby, now, (shake it up, baby) Twist and shout. (twist and shout) Cmon cmon, cmon, cmon, baby, now, (come on baby) Come on and work it on out. (work it on out)');
-		$(this._sleigh).addClass('flip').animate({
-			left: -150
-		}, 10000);
-	},
 
 	initServerEvents: function() {
 		this.sm = new JJ.managers.SocketManager();
